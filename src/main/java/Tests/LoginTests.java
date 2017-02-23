38994cba68527006ce20f1a3423fb6b6eb594d78
alohageck0 <royalfiish@gmail.com>
2017-02-23 11:58:19 -0800

    login test case

--- src/main/java/Tests/LoginTests.java
+++ src/main/java/Tests/LoginTests.java
@@ -3,6 +3,7 @@ package Tests;
 import Data.MySQLDataProviders;
 import ServiceClasses.TestTemplate;
 import objectRepo.HomePage;
+import objectRepo.Userpage;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -35,6 +36,7 @@ public class LoginTests extends TestTemplate {
 //   @Test(groups = {"login", "smoke"})
    public void login(String username, String password) {
       logger.info("login test case started");
+      getWebDriver().manage().window().maximize();
       getWebDriver().navigate().to("https://evernote.com/");
       logger.info("Home page opened");
 
@@ -46,13 +48,16 @@ public class LoginTests extends TestTemplate {
       logger.info("Sign in pop up opened");
 
       homePage.getUsernameTextfield().sendKeys(username);
-      logger.info("Username " + username + " enetered");
+      logger.info("Username " + username + " entered");
 
       homePage.getPasswordTextfield().sendKeys(password);
       logger.info("Password entered");
 
       homePage.getSignInButton().click();
       logger.info("Sign in button clicked");
+
+      Userpage userpage = new Userpage(getWebDriver());
+      Assert.assertEquals(userpage.getNotesTitle().getText(), "NOTES");
 
 
    }
---
+++ src/main/java/objectRepo/Userpage.java
@@ -0,0 +1,26 @@
+package objectRepo;
+
+import ServiceClasses.PageTemplate;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Userpage extends PageTemplate {
+   public Userpage(WebDriver driver) {
+      super(driver);
+   }
+
+   @FindBy(xpath = "html/body/div[2]/div[2]/div/div[1]/div[3]/div[1]/div/div/div[1]/div/div[1]/div/img[1]")
+   private WebElement userPicture;
+
+   @FindBy(xpath = "html/body/div[2]/div[2]/div/div[2]/div[3]/div[2]/div/div[1]/div/div/div")
+   private WebElement notesTitle;
+
+   public WebElement getUserPicture() {
+      return userPicture;
+   }
+
+   public WebElement getNotesTitle() {
+      return notesTitle;
+   }
+}
